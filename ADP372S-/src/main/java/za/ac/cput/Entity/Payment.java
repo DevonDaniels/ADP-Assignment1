89a1ba7e5d71b27d6cb921c59200b7718a7232e1
WEBDEV <devon.daniels16@gmail.com>
2022-03-31 14:11:38 -0700

    Entities done

--- ADP372S-/src/main/java/za/ac/cput/Entity/Payment.java
+++ ADP372S-/src/main/java/za/ac/cput/Entity/Payment.java
@@ -1,4 +1,120 @@
 package za.ac.cput.Entity;
 
+/*      Inventory.java
+        Entity for the Payment
+        Author: Devon Daniels (217299822)
+        Date: 03 April 2022
+
+ */
 public class Payment {
+    private String ID, Rental, CustomerId, StaffId, Amount, PaymentDate;
+
+    private Payment(){}
+
+    private Payment(Builder builder) {
+        this.ID = builder.ID;
+        this.Rental = builder.Rental;
+        this.CustomerId = builder.CustomerId;
+        this.StaffId = builder.StaffId;
+        this.Amount = builder.Amount;
+        this.PaymentDate =builder.PaymentDate;
+
+    }
+    public String getID() {
+        return ID;
+    }
+
+    public String getRental() {
+        return Rental;
+    }
+
+    public String getCustomerId() {
+        return CustomerId;
+    }
+
+    public String getStaffId() { return StaffId;}
+
+    public String getAmount() { return Amount;}
+
+    public String getPaymentDate() { return PaymentDate;}
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "za.ac.cput.Entity.Inventory{" +
+                "Id='" + ID + '\'' +
+                ", Rental='" + Rental + '\'' +
+                ", Customer ID='" + CustomerId + '\'' +
+                ", Staff ID='" + StaffId + '\'' +
+                ", Amount='" + Amount + '\'' +
+                ", Payment Date='" + PaymentDate + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String ID, Rental, CustomerId, StaffId, Amount, PaymentDate;
+
+        public Builder setID(String ID) {
+            this.ID = ID;
+
+            return this;
+        }
+
+        public Builder setRental(String Rental) {
+            this.Rental = Rental;
+
+            return this;
+        }
+
+        public Builder setCustomerId(String CustomerId) {
+            this.CustomerId = CustomerId;
+
+            return this;
+        }
+
+        public Builder setStaffId(String StaffId) {
+            this.StaffId = StaffId;
+
+            return this;
+        }
+
+        public Builder setAmount(String Amount) {
+            this.Amount = Amount;
+
+            return this;
+        }
+
+        public Builder setPaymentDate(String PaymentDate) {
+            this.PaymentDate = PaymentDate;
+
+            return this;
+        }
+
+
+
+
+        public Payment Build() {
+
+            return new Payment(this);
+        }
+
+        public Builder copy(Payment payment) {
+            this.ID = payment.ID;
+            this.Rental = payment.Rental;
+            this.CustomerId = payment.CustomerId;
+            this.StaffId = payment.StaffId;
+            this.Amount = payment.Amount;
+            this.PaymentDate = payment.PaymentDate;
+
+
+            return this;
+        }
+
+    }
 }
+
+
+
--- ADP372S-/src/main/java/za/ac/cput/Entity/Staff.java
+++ ADP372S-/src/main/java/za/ac/cput/Entity/Staff.java
@@ -1,4 +1,147 @@
 package za.ac.cput.Entity;
 
+/*      Inventory.java
+        Entity for the Staff
+        Author: Devon Daniels (217299822)
+        Date: 03 April 2022
+
+ */
 public class Staff {
+    private String ID, Payment, FirstName,LastName, Email, UserName, Password, LastUpdate, Picture;
+
+    private Staff(){}
+
+    private Staff(Builder builder) {
+        this.ID = builder.ID;
+        this.Payment = builder.Payment;
+        this.FirstName = builder.FirstName;
+        this.LastName = builder.LastName;
+        this.Email = builder.Email;
+        this.UserName =builder.UserName;
+        this.Password =builder.Password;
+        this.LastUpdate = builder.LastUpdate;
+        this.Picture =builder.Picture;
+    }
+    public String getID() {
+        return ID;
+    }
+
+    public String getPayment() {
+        return Payment;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() { return LastName;}
+
+    public String getEmail() { return Email;}
+
+    public String getUserName() { return UserName;}
+
+    public String getPassword() { return Password;}
+
+    public String getLastUpdate() { return LastUpdate;}
+
+    public String getPicture() { return Picture;}
+
+
+
+    @Override
+    public String toString() {
+        return "za.ac.cput.Entity.Inventory{" +
+                "Id='" + ID + '\'' +
+                ", Payment='" + Payment + '\'' +
+                ", FirstName='" + FirstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", Email='" + Email + '\'' +
+                ", UserName='" + UserName + '\'' +
+                ", Password='" + Password + '\'' +
+                ", Last Update='" + LastUpdate + '\'' +
+                ", Picture='" + Picture + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String ID, Payment, FirstName,LastName, Email, UserName, Password, LastUpdate, Picture;
+
+        public Builder setID(String ID) {
+            this.ID = ID;
+
+            return this;
+        }
+
+        public Builder setPayment(String Payment) {
+            this.Payment = Payment;
+
+            return this;
+        }
+
+        public Builder setFirstName(String FirstName) {
+            this.FirstName = FirstName;
+
+            return this;
+        }
+
+        public Builder setLastName(String LastName) {
+            this.LastName = LastName;
+
+            return this;
+        }
+
+        public Builder setEmail(String Email) {
+            this.Email = Email;
+
+            return this;
+        }
+
+        public Builder setUserName(String UserName) {
+            this.UserName = UserName;
+
+            return this;
+        }
+
+        public Builder setPassword(String Password) {
+            this.Password = Password;
+
+            return this;
+        }
+
+        public Builder setLastUpdate(String LastUpdate) {
+            this.LastUpdate = LastUpdate;
+
+            return this;
+        }
+
+        public Builder setPicture(String Picture) {
+            this.Picture = Picture;
+
+            return this;
+        }
+
+
+        public Staff Build() {
+
+            return new Staff(this);
+        }
+
+        public Builder copy(Staff staff) {
+            this.ID = staff.ID;
+            this.Payment = staff.Payment;
+            this.FirstName = staff.FirstName;
+            this.LastName = staff.LastName;
+            this.Email = staff.Email;
+            this.UserName =staff.UserName;
+            this.Password =staff.Password;
+            this.LastUpdate = staff.LastUpdate;
+            this.Picture =staff.Picture;
+
+            return this;
+        }
+
+    }
 }
+
+
+
