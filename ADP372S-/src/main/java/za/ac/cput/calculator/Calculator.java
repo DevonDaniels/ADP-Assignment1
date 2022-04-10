6ab21c1413d8a6aa7312e327c10891afc849a6a9
tashreeq-emeran <66222949+tashreeq-emeran@users.noreply.github.com>
2022-04-10 21:05:03 +0200

    Entity and Test Complete

---
+++ ADP372S-/src/main/java/za/ac/cput/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package za.ac.cput.calculator;
+
+/*      Calculator.java
+        Entity for Calculator
+        Author: Devon Daniels (217299822)
+        Date: 03 April 2022
+
+ */
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
